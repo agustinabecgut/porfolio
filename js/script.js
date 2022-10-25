fe33aeffa360d5cdfba55f0c899679ae32373335
agustinabecgut <agustina.b.becerra@gmail.com>
2022-10-25 17:23:32 -0300

    Clean Repo

---
+++ js/script.js
@@ -0,0 +1,89 @@
+function esconderTarjetas() {
+    document.getElementById("hab__tarjeta1").style.display = "none";
+    document.getElementById("hab__tarjeta2").style.display = "none";
+    document.getElementById("hab__tarjeta3").style.display = "none";
+
+    document.getElementById("hab__btn1-esconder").style.display = "none";
+    document.getElementById("hab__btn2-esconder").style.display = "none";
+    document.getElementById("hab__btn3-esconder").style.display = "none";
+
+    document.getElementById("formacion__tarjeta1").style.display = "none";
+    document.getElementById("formacion__tarjeta2").style.display = "none";
+    document.getElementById("formacion__tarjeta3").style.display = "none";
+
+    document.getElementById("formacion__btn1-esconder").style.display = "none";
+    document.getElementById("formacion__btn2-esconder").style.display = "none";
+    document.getElementById("formacion__btn3-esconder").style.display = "none";
+}
+
+function mostrarTarjetaHab1() {
+    document.getElementById("hab__btn1").style.display = "none";
+    document.getElementById("hab__btn1-esconder").style.display = "block";
+    document.getElementById("hab__tarjeta1").style.display = "block";
+}
+
+function mostrarTarjetaHab2() {
+    document.getElementById("hab__btn2").style.display = "none";
+    document.getElementById("hab__btn2-esconder").style.display = "block";
+    document.getElementById("hab__tarjeta2").style.display = "block";
+}
+
+function mostrarTarjetaHab3() {
+    document.getElementById("hab__btn3").style.display = "none";
+    document.getElementById("hab__btn3-esconder").style.display = "block";
+    document.getElementById("hab__tarjeta3").style.display = "block";
+}
+
+function esconderTarjetaHab1() {
+    document.getElementById("hab__btn1").style.display = "block";
+    document.getElementById("hab__btn1-esconder").style.display = "none";
+    document.getElementById("hab__tarjeta1").style.display = "none";
+}
+
+function esconderTarjetaHab2() {
+    document.getElementById("hab__btn2").style.display = "block";
+    document.getElementById("hab__btn2-esconder").style.display = "none";
+    document.getElementById("hab__tarjeta2").style.display = "none";
+}
+
+function esconderTarjetaHab3() {
+    document.getElementById("hab__btn3").style.display = "block";
+    document.getElementById("hab__btn3-esconder").style.display = "none";
+    document.getElementById("hab__tarjeta3").style.display = "none";
+}
+
+function mostrarTarjetaFormacion1() {
+    document.getElementById("formacion__btn1").style.display = "none";
+    document.getElementById("formacion__btn1-esconder").style.display = "block";
+    document.getElementById("formacion__tarjeta1").style.display = "block";
+}
+
+function mostrarTarjetaFormacion2() {
+    document.getElementById("formacion__btn2").style.display = "none";
+    document.getElementById("formacion__btn2-esconder").style.display = "block";
+    document.getElementById("formacion__tarjeta2").style.display = "block";
+}
+
+function mostrarTarjetaFormacion3() {
+    document.getElementById("formacion__btn3").style.display = "none";
+    document.getElementById("formacion__btn3-esconder").style.display = "block";
+    document.getElementById("formacion__tarjeta3").style.display = "block";
+}
+
+function esconderTarjetaFormacion1() {
+    document.getElementById("formacion__btn1").style.display = "block";
+    document.getElementById("formacion__btn1-esconder").style.display = "none";
+    document.getElementById("formacion__tarjeta1").style.display = "none";
+}
+
+function esconderTarjetaFormacion2() {
+    document.getElementById("formacion__btn2").style.display = "block";
+    document.getElementById("formacion__btn2-esconder").style.display = "none";
+    document.getElementById("formacion__tarjeta2").style.display = "none";
+}
+
+function esconderTarjetaFormacion3() {
+    document.getElementById("formacion__btn3").style.display = "block";
+    document.getElementById("formacion__btn3-esconder").style.display = "none";
+    document.getElementById("formacion__tarjeta3").style.display = "none";
+}
